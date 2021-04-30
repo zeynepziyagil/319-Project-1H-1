53b4ceed927af4d8ca0585fb4d5117ecbe6d0654
Bilgehan Akcan <b.akcan013@gmail.com>
2021-05-01 00:36:21 +0300

    Menu is created

--- Interface/src/Components/Menu.jsx
+++ Interface/src/Components/Menu.jsx
@@ -2,8 +2,8 @@ import React from "react"
 import {AiOutlineBell} from "react-icons/ai";
 import {BiMessageDetail} from "react-icons/bi";
 import {CgProfile} from "react-icons/cg";
-import {AiOutlineMenu} from "react-icons/ai";
 import {Link} from "react-router-dom";
+import NavigationBar from "./NavigationBar";
 
 var whoseProfile = "instructor";
 
@@ -17,7 +17,7 @@ function Menu() {
                     <td>
                         {whoseProfile === "instructor" ? <Link to="StudentProfile"><CgProfile size="3em" /></Link> : <Link to="Dashboard"><CgProfile size="3em" /></Link>}
                     </td>
-                    <td><AiOutlineMenu size="3em" /></td>
+                    <td><NavigationBar /></td>
                 </tr>
             </table>
         </div>
