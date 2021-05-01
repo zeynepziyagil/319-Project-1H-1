489414a4d9c37e0c910e60663465ff138f5257a4
Bilgehan Akcan <b.akcan013@gmail.com>
2021-05-01 14:08:37 +0300

    Message page is finished

--- Interface/src/Components/Menu.jsx
+++ Interface/src/Components/Menu.jsx
@@ -5,7 +5,7 @@ import {CgProfile} from "react-icons/cg";
 import {Link} from "react-router-dom";
 import NavigationBar from "./NavigationBar";
 
-var whoseProfile = "instructor";
+var whoseProfile = "student";
 
 function Menu() {
     return (
@@ -13,9 +13,9 @@ function Menu() {
             <table className="icon">
                 <tr >
                     <td><AiOutlineBell size="3em" /></td>
-                    <td><BiMessageDetail size="3em" /></td>
+                    <td><Link to="MessagePage"> <BiMessageDetail size="3em" /></Link></td>
                     <td>
-                        {whoseProfile === "instructor" ? <Link to="StudentProfile"><CgProfile size="3em" /></Link> : <Link to="Dashboard"><CgProfile size="3em" /></Link>}
+                        {whoseProfile === "student" ? <Link to="StudentProfile"><CgProfile size="3em" /></Link> : <Link to="Dashboard"><CgProfile size="3em" /></Link>}
                     </td>
                     <td><NavigationBar /></td>
                 </tr>
