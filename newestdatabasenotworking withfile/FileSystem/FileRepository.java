83524d211406c3a59c681898a30a91c248aed00c
İlke Kaş <78687712+ilke-kas@users.noreply.github.com>
2021-05-02 01:11:27 +0300

    Delete newestdatabasenotworking withfile directory

--- newestdatabasenotworking withfile/FileSystem/FileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.catchup.FileSystem;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-@Transactional
-public interface FileRepository extends JpaRepository<FileEntity, String> {
-}
